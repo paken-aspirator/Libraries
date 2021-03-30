906eb173cb049ecb060d53605fb573d7e9c4e2b7
aspirator <62785202+paken-aspirator@users.noreply.github.com>
2021-03-30 21:42:37 +0900

    Update recursive_lazysegtree.cpp

--- recursive_lazysegtree.cpp
+++ recursive_lazysegtree.cpp
@@ -10,7 +10,7 @@ template <class S,
 	F(*composition)(F, F),
 	F(*id)()>
 	class lazy_segtree {
-	int size, limit, depth, queryl, queryr;
+	int size, limit, depth, queryl = 0, queryr = 0;
 	std::vector<S>data;
 	std::vector<F>lazy;
 	void update(int k) { data[k] = op(data[2 * k], data[2 * k + 1]); }
@@ -48,65 +48,47 @@ template <class S,
 		}
 	}
 	public:
-		lazy_segtree(int N) :size(N) {
-			assert(0 <= N);
-			depth = 0;
-			while ((1 << depth) < size)depth++;
-			limit = (1 << depth);
-			data.resize(limit * 2, e());
-			lazy.resize(limit * 2, id());
-		}
-		lazy_segtree(const std::vector<S>& v) :size((int)v.size()) {
-			limit = 1;
-			while (limit < size)limit <<= 1;
-			data.resize(limit * 2, e());
-			lazy.resize(limit * 2, id());
-			for (int i = 0; i < size; i++) data[i + limit] = v[i];
-			for (int i = limit - 1; i > 0; i--) update(i);
-		}
-		void set(int pos, S x) {
-			assert(0 <= pos && pos < size);
-			pos += limit;
-			for (int i = depth; i >= 0; depth--)eval(pos >> i);
-			data[pos] = x;
-			for (pos >>= 1; pos > 0; pos >>= 1) update(pos);
-		}
-		S get(int pos) {
-			assert(0 <= pos && pos < size);
-			pos += limit;
-			for (int i = depth; i >= 0; i--)eval(pos >> i);
-			return data[pos];
-		}
-		void apply(int a, int b, F x) {
-			assert(0 <= a && a <= b && b < size);
-			if (a == b)return;
-			queryl = a; queryr = b;
-			apply(x, 1, 0, limit);
-		}
-		S prod(int a, int b) {
-			assert(0 <= a && a <= b && b < size);
-			if (a == b)return e();
-			queryl = a; queryr = b;
-			return prod(1, 0, limit);
-		}
-		void debug() {
-			rep(i, limit) {
-				cout <<right<<setw(2)<< data[i] << " ";
-			}
-			cout << endl;
-			for (int i = limit; i < 2 * limit; i++) {
-				cout << right << setw(2) << data[i] << " ";
-			}
-			cout << endl;
-			rep(i, limit) {
-				cout << right << setw(2) << lazy[i] << " ";
-			}
-			cout << endl;
-			for (int i = limit; i < 2 * limit; i++) {
-				cout << right << setw(2) << lazy[i] << " ";
-			}
-			cout << endl;
-		}
+	lazy_segtree(int N) :size(N) {
+		assert(0 <= N);
+		depth = 0;
+		while ((1 << depth) < size)depth++;
+		limit = (1 << depth);
+		data.resize(limit * 2, e());
+		lazy.resize(limit * 2, id());
+	}
+	lazy_segtree(const std::vector<S>& v) :size((int)v.size()) {
+		limit = 1;
+		while (limit < size)limit <<= 1;
+		data.resize(limit * 2, e());
+		lazy.resize(limit * 2, id());
+		for (int i = 0; i < size; i++) data[i + limit] = v[i];
+		for (int i = limit - 1; i > 0; i--) update(i);
+	}
+	void set(int pos, S x) {
+		assert(0 <= pos && pos < size);
+		pos += limit;
+		for (int i = depth; i >= 0; i--)eval(pos >> i);
+		data[pos] = x;
+		for (pos >>= 1; pos > 0; pos >>= 1) update(pos);
+	}
+	S get(int pos) {
+		assert(0 <= pos && pos < size);
+		pos += limit;
+		for (int i = depth; i >= 0; i--)eval(pos >> i);
+		return data[pos];
+	}
+	void apply(int a, int b, F x) {
+		assert(0 <= a && a <= b && b <= size);
+		if (a == b)return;
+		queryl = a; queryr = b;
+		apply(x, 1, 0, limit);
+	}
+	S prod(int a, int b) {
+		assert(0 <= a && a <= b && b <= size);
+		if (a == b)return e();
+		queryl = a; queryr = b;
+		return prod(1, 0, limit);
+	}
 };
 /*
 int op(int x, int y) { return max(x, y); }
