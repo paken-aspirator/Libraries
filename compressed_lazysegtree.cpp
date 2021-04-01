d7f260b477b8a702148ed79ecdf1eefd137ab712
aspirator <62785202+paken-aspirator@users.noreply.github.com>
2021-04-01 11:13:27 +0900

    Update compressed_lazysegtree.cpp

--- compressed_lazysegtree.cpp
+++ compressed_lazysegtree.cpp
@@ -21,26 +21,25 @@ template <class S,
 			child[1] = nullptr;
 		}
 	};
-	node* root = nullptr;
+	node* root = new node();
 	int limit, depth, queryl = 0, queryr = 0;
-	S scopy;
-	F fcopy;
-
+	S scopy; F fcopy;
+	
 	S getsum(node* np) { return np ? np->sum : e(); }
-	void eval(node* np, bool bo) {
+	void eval(node*& np, bool bo) {
 		if (np->lazy == id())return;
 		if (bo) {
-			if (!np->child[0]) np->child[0] = new node;
+			if (!np->child[0]) np->child[0] = new node();
 			np->child[0]->lazy = composition(np->lazy, np->child[0]->lazy);
-			if (!np->child[1])np->child[1] = new node;
+			if (!np->child[1])np->child[1] = new node();
 			np->child[1]->lazy = composition(np->lazy, np->child[1]->lazy);
 		}
 		np->sum = mapping(np->lazy, np->sum);
 		np->lazy = id();
 	}
-	void set(node*& np, int& pos, int dep) {
-		if (!np) np = new node;
-		eval(np, 0 <= dep);
+	void set(node*& np, int pos, int dep) {
+		if (!np) np = new node();
+		eval(np, 0 >= dep);
 		if (dep == -1) {
 			np->sum = scopy;
 			return;
@@ -50,18 +49,17 @@ template <class S,
 		return;
 	}
 	void apply(node*& np, int l, int r) {
-		if (!np)np = new node;
+		if (!np)np = new node();
+		eval(np, r - l > 1);
 		if (queryl <= l && r <= queryr) {
 			np->lazy = composition(fcopy, np->lazy);
 			eval(np, r - l > 1);
 		}
 		else if (queryl < r && l < queryr) {
-			eval(np, r - l > 1);
 			apply(np->child[0], l, (l + r) / 2);
 			apply(np->child[1], (l + r) / 2, r);
 			np->sum = op(getsum(np->child[0]), getsum(np->child[1]));
 		}
-		eval(np, r - l > 1);
 	}
 	S prod(node* np, int l, int r) {
 		if (r <= queryl || queryr <= l || !np) {
@@ -79,7 +77,6 @@ template <class S,
 	public:
 	compressed_lazysegtree(int N) {
 		assert(0 < N);
-		root = new node;
 		depth = 0;
 		while ((1U << depth) < (unsigned int)(N)) depth++;
 		limit = 1 << depth;
@@ -93,11 +90,11 @@ template <class S,
 		assert(0 <= pos && pos < limit);
 		node* np = root;
 		for (int i = depth - 1; i >= 0; i--) {
-			if (!np)return e();
 			eval(np, true);
 			np = np->child[(bool)(pos & (1 << i))];
+			if (!np)return e();
 		}
-		if (!np)return e();
+		eval(np, false);
 		return np->sum;
 	}
 	void apply(int l, int r, F x) {
