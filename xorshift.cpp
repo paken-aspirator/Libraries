c82ab394e0159bb534b8f11be2130f725a13fc0f
aspirator <62785202+paken-aspirator@users.noreply.github.com>
2021-04-24 00:01:51 +0900

    Update xorshift.cpp

--- xorshift.cpp
+++ xorshift.cpp
@@ -7,7 +7,7 @@ std::random_device seed_gen;
 class xorshift {
 	uint64_t seed;
 public:
-	xorshift() { seed = uint64_t(seed_gen()) + seed_gen(); }
+	xorshift() { seed = (uint64_t(seed_gen()) << 32) + seed_gen(); }
 	inline uint64_t get64() {
 		seed ^= (seed << 13); seed ^= (seed >> 7);
 		return seed ^= (seed << 17);
