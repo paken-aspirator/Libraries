2d4bb611abf12e73ac5cb6221002a0e934eedf5e
aspirator <62785202+paken-aspirator@users.noreply.github.com>
2021-05-21 23:42:14 +0900

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -17,18 +17,19 @@ struct edge {
 template<class T>bool chmin(T& xx, T yy) { if (xx > yy) { xx = yy; return true; }return false; }
 
 namespace Library {
-	template<class T>vector<T>dijkstra(vector<vector<edge>>& graph, int start) {//到達不可能な時はINFを返す
+	template<class T, T INF>vector<T>dijkstra(vector<vector<edge>>& graph, int start) {//到達不可能な時はINFを返す
 		assert(0 <= start && start < graph.size());
-		priority_queue<pair<int, T>, vector<pair<int, T>>, greater<pair<int, T>>>que;
-		que.push({ start,0 });
-		vector<T>dist(graph.size(), 4611686015206162431);
+		priority_queue<pair<T, int>, vector<pair<T, int>>, greater<pair<T, int>>>que;
+		que.push(make_pair(0, start));
+		vector<T>dist(graph.size(), INF);
 		dist[start] = 0;
 		while (!que.empty()) {
-			pair<int, T>front = que.top(); que.pop();
-			if (front.second > dist[front.first])continue;
-			for (edge& p : graph[front.first]) {
-				if (chmin(dist[p.to], front.second + p.cost)) {
-					que.push(make_pair(p.to, dist[p.to]));
+			pair<T, int>p = que.top();
+			que.pop();
+			if (dist[p.second] < p.first)continue;
+			for(edge& e : graph[p.second]) {
+				if (chmin(dist[e.to], p.first + e.cost)) {
+					que.push(make_pair(dist[e.to], e.to));
 				}
 			}
 		}
