b1faac53e0be7294a0c737520abd26e2fdc7ca90
aspirator <62785202+paken-aspirator@users.noreply.github.com>
2021-09-10 19:36:54 +0900

    Update xorshift.cpp

--- xorshift.cpp
+++ xorshift.cpp
@@ -12,8 +12,12 @@ public:
 		seed ^= (seed << 13); seed ^= (seed >> 7);
 		return seed ^= (seed << 17);
 	}
+	// [0, 2^64-1)
 	inline uint32_t operator()() { return get64(); }
+	// [0, r)
 	inline uint32_t operator()(uint32_t r) { return operator()() % r; }
+	// [mi, ma)
 	inline int operator()(int mi, int ma) { return mi + operator()(ma - mi); }
+	// [0,1)
 	inline double prob() { return double(operator()()) / 0xffffffff; }
 };
